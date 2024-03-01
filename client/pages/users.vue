4335feb0cff7f2747929a95bae34a62fea521a1f
SCJAB <asheancecyl@gmail.com>
2024-03-01 21:27:58 +0800

    new

--- client/pages/users.vue
+++ client/pages/users.vue
@@ -1,72 +1,73 @@
-    <template>
-        <div class="h-screen bg-gradient-to-r from-purple-100 via-pink-100 to-blue-100">
+<template>
+    <div class="h-screen bg-gradient-to-r from-purple-100 via-pink-100 to-blue-100">
         <div class="w-full flex justify-center">
             <table v-if="state.user" class="w-9/12 border-collapse mt-4">
-            <thead>
-            <tr class="bg-slate-900 border text-white">
-                <th class="border-slate-900 border px-4 py-2">Name</th>
-                <th class="border-slate-900 border px-4 py-2">Email</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="user in state.user" :key="user.id" class="border">
-                <td class="border-slate-900 border px-4 py-2">{{ user.name }}</td>
-                <td class="border-slate-900 border px-4 py-2">{{ user.email }}</td>
-            </tr>
-            </tbody>
-        </table>
+                <thead>
+                <tr class="bg-slate-900 border text-white">
+                    <th class="border-slate-900 border px-4 py-2">Name</th>
+                    <th class="border-slate-900 border px-4 py-2">Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="user in state.user" :key="user.id" class="border">
+                    <td class="border-slate-900 border px-4 py-2">{{ user.name }}</td>
+                    <td class="border-slate-900 border px-4 py-2">{{ user.email }}</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
         <div v-if="state.user" class="w-9/12 mx-48 mt-5">
             <button type="button" @click="goBack">Back</button>
         </div>
-        </div>
-    </template>
+    </div>
+</template>
 
-    <script setup>
-    const state = reactive({
-        user: null,
-    })
+<script setup>
 
-    onMounted(() => {
-        fetchUser()
-    })
+const state = reactive({
+    user: null,
+})
 
-    onBeforeRouteLeave((to, from, next) => {
-    if (state.user) {
-        logout();
-    }
-    next()
-    })
+onMounted(() => {
+    fetchUser()
+})
 
-    async function fetchUser() {
-        try {
-            const response = await $fetch (`http://127.0.0.1:8000/api/user`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('_token')
-                }
-            })
+onBeforeRouteLeave((to, from, next) => {
+if (state.user) {
+    logout();
+}
+next();
+})
 
-            if (response.data) {
-                state.user = response.data
+async function fetchUser() {
+    try {
+        const response = await $fetch (`http://127.0.0.1:8000/api/user`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('_token')
             }
-        } catch (error) {
-            state.errors = error.response
-            console.log('Error fetching users', error)
-        }
-    }
+        })
 
-    function logout() {
+        if (response.data) {
+            state.user = response.data
+        }
+    } catch (error) {
+        state.errors = error.response
+        console.log('Error fetching users', error)
+    }
+}
+
+function logout() {
     localStorage.removeItem('_token')
     state.user = null
-    }
+}
 
-    function goBack() {
+function goBack() {
     console.log('Going back...');
     navigateTo('/login');
-    }
-    </script>
+}
+</script>
 
-    <style scoped>
+<style scoped>
 
-    </style> 
+</style> 
