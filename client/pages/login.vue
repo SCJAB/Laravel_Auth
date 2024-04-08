0102f6da6e68656fbf76d7e83e15a38e38df5e20
SCJAB <asheancecyl@gmail.com>
2024-04-09 00:23:47 +0800

    veevalidate

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,25 +1,27 @@
 <template>
     <div>
         <div class="flex items-center justify-center h-screen bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200">
-            <form @submit.prevent="submitForm()">
+            <Form @submit.prevent="submitForm()">
                 <p class="text-center text-2xl font-bold">Login</p>
                 <div class="bg-black bg-opacity-10 p-10 rounded-3xl text-black shadow-2xl shadow-violet-700 space-y-6">
                     <div>
                         <label for="email_address">Email Address</label>
                         <div>
-                            <input class="rounded p-1 text-violet-800 font-semibold" type="email" name="email" placeholder="email" v-model="state.user.email">
+                            <Field class="rounded p-1 text-violet-800 font-semibold" type="email" name="email" placeholder="email" v-model="state.user.email" :rules="validateEmail" />
                             <p class="text-red-700">
                                 {{ state.errors && state.errors._data && state.errors._data.errors && state.errors._data.errors.email && state.errors._data.errors.email[0] }}
                             </p>
+                            <ErrorMessage name="email" class="text-red-600"/>
                         </div>
                     </div>
                     <div>
                         <label for="password">Password</label>
                         <div>
-                            <input class="rounded p-1 text-violet-800 font-semibold" type="password" name="password" placeholder="password" v-model="state.user.password">
+                            <Field class="rounded p-1 text-violet-800 font-semibold" type="password" name="password" placeholder="password" v-model="state.user.password" :rules="validatePassword" />
                             <p class="text-red-700">
                                 {{ state.errors && state.errors._data && state.errors._data.errors && state.errors._data.errors.password && state.errors._data.errors.password[0] }}
                             </p>
+                            <ErrorMessage name="password" class="text-red-600"/>
                         </div>
                     </div>
                     <div class="flex justify-between">
@@ -30,12 +32,35 @@
                 <div class="mt-5 flex justify-center underline" >
                     <button type="button" @click="goToRegister">Create Account</button>
                 </div>
-            </form>
+            </Form>
         </div>
     </div>
 </template>
 
 <script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+
+const validateEmail = (value) => {
+    // if the field is empty
+    if (!value) {
+    return 'This field is required';
+    }
+    // if the field is not a valid email
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if (!regex.test(value)) {
+    return 'This field must be a valid email';
+    }
+    return true;
+};
+
+const validatePassword = (value) => {
+    // if the field is empty
+    if (!value) {
+    return 'This field is required';
+    }
+    return true;
+};
+
 const state = reactive({
     errors: null,
     isPageLoading: false,
--- client/pages/register.vue
+++ client/pages/register.vue
@@ -1,34 +1,37 @@
 <template>
     <div>
         <div class="flex items-center justify-center h-screen bg-gradient-to-r from-pink-200 via-purple-200 to-blue-200 p-8">
-            <form @submit.prevent="submitForm()">
+            <Form @submit.prevent="submitForm()">
                 <p class="text-center text-2xl font-bold">Sign Up</p>
                 <div class="bg-black bg-opacity-10 p-10 rounded-3xl text-black shadow-2xl shadow-violet-700 space-y-6">
                     <div>
                         <label for="name">Name</label>
                         <div>
-                            <input class="rounded p-1 text-violet-800 font-semibold" type="text" name="name" placeholder="name" v-model="state.user.name">
+                            <Field class="rounded p-1 text-violet-800 font-semibold" type="text" name="name" placeholder="name" v-model="state.user.name" :rules="isRequired" />
                             <p class="text-red-700">
                                 {{ state.errors && state.errors._data && state.errors._data.errors && state.errors._data.errors.email && state.errors._data.errors.email[0] }}
                             </p>
+                            <ErrorMessage name="name" class="text-red-600"/>
                         </div>
                     </div>
                     <div>
                         <label for="email_address">Email Address</label>
                         <div>
-                            <input class="rounded p-1 text-violet-800 font-semibold" type="email" name="email" placeholder="email address" v-model="state.user.email">
+                            <Field class="rounded p-1 text-violet-800 font-semibold" type="email" name="email" placeholder="email address" v-model="state.user.email" :rules="validateEmail" />
                             <p class="text-red-700">
                                 {{ state.errors && state.errors._data && state.errors._data.errors && state.errors._data.errors.email && state.errors._data.errors.email[0] }}
                             </p>
+                            <ErrorMessage name="email" class="text-red-600"/>
                         </div>
                     </div>
                     <div>
                         <label for="password">Password</label>
                         <div>
-                            <input class="rounded p-1 text-violet-800 font-semibold" type="password" name="password" placeholder="password" v-model="state.user.password">
+                            <Field class="rounded p-1 text-violet-800 font-semibold" type="password" name="password" placeholder="password" v-model="state.user.password" :rules="validatePassword" />
                             <p class="text-red-700">
                                 {{ state.errors && state.errors._data && state.errors._data.errors && state.errors._data.errors.password && state.errors._data.errors.password[0] }}
                             </p>
+                            <ErrorMessage name="password" class="text-red-600"/>
                         </div>
                     </div>
                     <div class="flex justify-between">
@@ -39,12 +42,43 @@
                 <div class="mt-5 flex justify-center underline" >
                     <button type="button" @click="goToLogin">Already have an Account?</button>
                 </div>
-            </form>
+            </Form>
         </div>
     </div>
 </template>
 
 <script setup>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+
+const isRequired = (value) => {
+    if(value&&value.trim()) {
+        return true;
+    }
+
+    return 'This is required';
+}
+
+const validateEmail = (value) => {
+    // if the field is empty
+    if (!value) {
+    return 'This field is required';
+    }
+    // if the field is not a valid email
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if (!regex.test(value)) {
+    return 'This field must be a valid email';
+    }
+    return true;
+};
+
+const validatePassword = (value) => {
+    // if the field is empty
+    if (!value) {
+    return 'This field is required';
+    }
+    return true;
+};
+
 const state = reactive({
     errors: null,
     isPageLoading: false,
